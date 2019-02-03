e36ce2dd29830f0d95b454a0f9ae5b630a692e91
decstor <47300592+decstor@users.noreply.github.com>
2019-02-03 20:03:11 +0200

    Update 1.php

--- 1.php
+++ 1.php
@@ -19,17 +19,35 @@ if ($summ <= 100000) {
     $five = (int)(($summ - $fivehung * 500 - $twohung * 200 - $onehung * 100 - $fifty * 50 - $twenty * 20 - $ten * 10) / 5);
     $two = (int)(($summ - $fivehung * 500 - $twohung * 200 - $onehung * 100 - $fifty * 50 - $twenty * 20 - $ten * 10 - $five * 5) / 2);
     $one = (int)(($summ - $fivehung * 500 - $twohung * 200 - $onehung * 100 - $fifty * 50 - $twenty * 20 - $ten * 10 - $five * 5 - $two * 2) / 1);
+    if ($fivehung > 0) {
+        echo "500: {$fivehung}" . PHP_EOL;
+    }
 
-    echo "500: {$fivehung}" . PHP_EOL;
-    echo "200: {$twohung}" . PHP_EOL;
-    echo "100: {$onehung}" . PHP_EOL;
-    echo "50: {$fifty}" . PHP_EOL;
-    echo "20: {$twenty}" . PHP_EOL;
-    echo "10: {$ten}" . PHP_EOL;
-    echo "5: {$five}" . PHP_EOL;
-    echo "2: {$two}" . PHP_EOL;
-    echo "1: {$one}" . PHP_EOL;
+    if ($twohung > 0){
+        echo "200: {$twohung}" . PHP_EOL;
+    }
+    if ($onehung > 0) {
+        echo "100: {$onehung}" . PHP_EOL;
+    }
+    if ($fifty > 0) {
+        echo "50: {$fifty}" . PHP_EOL;
+    }
+    if ($twenty > 0) {
+        echo "20: {$twenty}" . PHP_EOL;
+    }
+    if ($ten > 0) {
+        echo "10: {$ten}" . PHP_EOL;
+    }
+    if ($five > 0) {
+        echo "5: {$five}" . PHP_EOL;
+    }
+    if ($two > 0) {
+        echo "2: {$two}" . PHP_EOL;
+    }
+    if ($one > 0) {
+        echo "1: {$one}" . PHP_EOL;
+    }
 }
 else {
-echo "Введите число которое <=100000 грн.";
+    echo "Введите число которое <=100000 грн.";
 }
